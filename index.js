78080e4536b08c626efbe3b35411956566e4eb84
Alina <135998117+laviete@users.noreply.github.com>
2023-07-19 10:00:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,22 +18,16 @@ function ProductPrototype() {
   //setSaleToPrice(value=0) - встановлює знижку і повертає нову ціну товару, з урахуванням знижки
   this.setSaleToPrice = function (value = 0) {
     this.discount = value;
-    if (validateNumber(this.discount) && validateNumber(this.price)) {
-      return this.price - this.price * (this.discount / 100);
+    if (validateNumber(value)) {
+      return this.price = (this.price - this.price * (this.discount / 100));
     }
     return null;
   };
   //buyAmountProduct(amountValue) - повертає загальну вартість, якщо є достатня кількість, якщо не має повертаєте - null
   this.buyAmountProduct = function (amountValue) {
-    if (
-      validateNumber(amountValue) &&
-      validateNumber(this.price) &&
-      validateNumber(this.discount) &&
-      amountValue <= this.amount ) {
-      return `
-    Without discount: ${amountValue * this.price}
-    With discount: ${amountValue * (this.price - this.price * (this.discount / 100))}
-      `;
+    if (validateNumber(amountValue) && amountValue <= this.amount) {
+      this.amount -= amountValue;
+      return amountValue * this.price;
     }
     return null;
   };
@@ -51,6 +45,12 @@ const product1 = new Product("Water", 430, 200);
 console.log(product1.showInfo());
 console.log(product1.setSaleToPrice(50));
 console.log(product1.buyAmountProduct(30));
+
+const product2 = new Product("Flour", 750, 150);
+console.log(product2.showInfo());
+console.log(product2.setSaleToPrice(20));
+console.log(product2.buyAmountProduct(3));
+
 
 const product2 = new Product("Flour", 750, 150);
 console.log(product2.showInfo());
